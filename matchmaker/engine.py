c905a3af6c5ba7b76443c453f572374a30a0f615
jcsalterego <github@jcsalterego.otherinbox.com>
2009-08-02 07:56:21 -0500

    Fake engine with randomized recommendations

--- matchmaker/engine.py
+++ matchmaker/engine.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python
 
+import random
+from collections import defaultdict
+
 class Engine:
     def __init__(self, database):
         """Constructor
         """
         self.database = database
+        self.recommended = defaultdict(list)
+        self.process()
+
+    def process(self):
+        db = self.database
+
+        r_list = db.r_info.keys()
+        for u in db.test_u:
+            random.shuffle(r_list)
+            self.recommended[u] = [str(x) for x in r_list[:10]]
+
+    def results(self):
+        lines = []
+        for u, r_list in self.recommended.items():
+            lines.append(':'.join((str(u), ','.join(r_list))))
+        return "\n".join(lines)
--- recommend.py
+++ recommend.py
@@ -2,11 +2,31 @@
 
 import sys
 from matchmaker.database import *
+from matchmaker.engine import *
 
 def main(argv):
+    if 'production' in argv:
+        return production(argv)
+    else:
+        return testing(argv)
+
+def production(argv):
+    db = Database('data')
+    e = Engine(db)
+    results = e.results()
+
+    resf = open('results.txt', 'w')
+    resf.write(results)
+    resf.close()
+    return 0
+
+def testing(argv):
     db = Database('minidata')
     if 'stats' in argv:
         db.summary()
+    e = Engine(db)
+    print(e.results())
+    return 0
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
