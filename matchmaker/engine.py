e8d3bdf4dd99ec65fd019e5dd552e05d5d5a5d9d
jcsalterego <github@jcsalterego.otherinbox.com>
2009-08-16 16:59:39 -0500

    Only for more than 2500 scores; #105

--- matchmaker/engine.py
+++ matchmaker/engine.py
@@ -211,7 +211,7 @@ class Engine:
         for i in sorted(purge, reverse=True):
             del scores[i]
 
-        if len(scores) > 1500:
+        if len(scores) > 2500:
             output = []
             mean = sum([x[1] for x in scores]) / len(scores)
             std_dev = (sum([(x[1] - mean) ** 2
