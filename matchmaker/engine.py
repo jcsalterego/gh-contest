9b0dcb41b86328f57dd7046482974f66d570a8ff
jcsalterego <github@jcsalterego.otherinbox.com>
2009-08-17 18:27:26 -0500

    LIMIT 5 to LIMIT 10 for r_matrix query; #112

--- matchmaker/engine.py
+++ matchmaker/engine.py
@@ -118,14 +118,14 @@ class Engine:
                        "FROM r_matrix2 "
                        "WHERE r1=%d "
                        "ORDER BY val DESC "
-                       "LIMIT 5")
+                       "LIMIT 10")
                       % r)
             results += list(c.fetchall())
             c.execute(("SELECT r1, val "
                        "FROM r_matrix2 "
                        "WHERE r2=%d "
                        "ORDER BY val DESC "
-                       "LIMIT 5")
+                       "LIMIT 10")
                       % r)
             results += list(c.fetchall())
             results_ = {}
