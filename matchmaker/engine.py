1d56479954eb4a888009b858f24d986d481c00d7
jcsalterego <github@jcsalterego.otherinbox.com>
2009-08-21 10:57:59 -0500

    Different u_matrix_fwd r1 addition; #137

--- matchmaker/engine.py
+++ matchmaker/engine.py
@@ -135,7 +135,7 @@ class Engine:
         for u1, u_val in results[:3]:
             if u1 in u_watching:
                 for r1 in u_watching[u1]:
-                    scores[r1] += u_val
+                    scores[r1] += log(u_val, 10)
 
         for r in u_watching[user]:
             # loop through all watched repositories
